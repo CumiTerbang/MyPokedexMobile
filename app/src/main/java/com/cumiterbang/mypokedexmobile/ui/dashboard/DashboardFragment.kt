db61fc32c8c1afca04e5864e724dfb20f584df06
Haryo Pramanto <gitarringsek@gmail.com>
2023-10-21 04:00:22 +0700

    first commit
    first commit

---
+++ app/src/main/java/com/cumiterbang/mypokedexmobile/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,42 @@
+package com.cumiterbang.mypokedexmobile.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.cumiterbang.mypokedexmobile.databinding.FragmentCatchBinding
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentCatchBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentCatchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textCatchMenu
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
