6905b5053896eb953f3bc5f28424d10e0eb1df2d
Haryo Pramanto <gitarringsek@gmail.com>
2023-10-21 12:08:06 +0700

    update main activity

--- app/src/main/java/com/cumiterbang/mypokedexmobile/MainActivity.kt
+++ app/src/main/java/com/cumiterbang/mypokedexmobile/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.cumiterbang.mypokedexmobile
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.cumiterbang.mypokedexmobile.databinding.ActivityMainBinding
 
@@ -18,16 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home, R.id.navigation_catch, R.id.navigation_info
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
+    private fun setupBottomNavigation(){
+        with(binding.navView){
+            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+            this.setupWithNavController(navController)
+        }
     }
 }
