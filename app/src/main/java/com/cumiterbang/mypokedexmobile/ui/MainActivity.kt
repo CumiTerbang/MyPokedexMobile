e28e66f423b25321b4c695b8357489f36373318e
Haryo Pramanto <gitarringsek@gmail.com>
2023-10-23 10:28:45 +0700

    update exit splashscreen animation

--- app/src/main/java/com/cumiterbang/mypokedexmobile/ui/MainActivity.kt
+++ app/src/main/java/com/cumiterbang/mypokedexmobile/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.cumiterbang.mypokedexmobile.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -10,6 +14,7 @@ import com.cumiterbang.mypokedexmobile.R
 import com.cumiterbang.mypokedexmobile.databinding.ActivityMainBinding
 import com.cumiterbang.mypokedexmobile.ui.home_menu.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -19,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
+        animateExitSplashSceen(splashScreen)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -41,6 +47,26 @@ class MainActivity : AppCompatActivity() {
                 binding.navView.selectedItemId = R.id.navigation_catch
                 homeViewModel.setGoCactchPokemon(false)
             }
+        }
+    }
+
+    private fun animateExitSplashSceen(splashscreen: SplashScreen){
+        splashscreen.setOnExitAnimationListener { splashScreenView ->
+            val anim = ValueAnimator.ofFloat(1f, 7f)
+            anim.duration = 500
+            anim.addUpdateListener { animation ->
+                splashScreenView.view.setScaleX(animation.animatedValue as Float)
+                splashScreenView.view.setScaleY(animation.animatedValue as Float)
+            }
+
+            anim.addListener(object:AnimatorListenerAdapter(){
+                override fun onAnimationEnd(animation: Animator) {
+                    splashScreenView.remove()
+                }
+            })
+
+            Thread.sleep(1000)
+            anim.start()
         }
     }
 }
